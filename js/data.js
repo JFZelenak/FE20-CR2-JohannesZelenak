3a58d8273fce96c12ddfe482bd6cb59eae557734
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-06 16:13:47 +0200

    add task strings

--- js/data.js
+++ js/data.js
@@ -1,45 +1,63 @@
 let tasks = `
     [{
-        "taskName": "0001",
+        "taskName": "Wash the car",
         "image": "./images/walk-dog.jpg",
-        "description": "You need to take Fluffy for a walk. He needs to run and be with other dogs!",
+        "description": "The car needs to be washed this week. It has been a while ... So do it!",
+        "priority": 0,
+        "deadline": "15.10.2023"
+    },{
+        "taskName": "Clean the appartment",
+        "image": "./images/walk-dog.jpg",
+        "description": "As usual the apartment should be cleaned. Come on, do it!",
+        "priority": 0,
+        "deadline": "13.10.2023"
+    },{
+        "taskName": "Buy food stuff",
+        "image": "./images/walk-dog.jpg",
+        "description": "The refrigorator is empty. Soon there will be nothing to eat. So go buy some food stuff!",
+        "priority": 0,
+        "deadline": "10.10.2023"
+    },{
+        "taskName": "Call grandma",
+        "image": "./images/walk-dog.jpg",
+        "description": "You haven't talked to here in a while. So just call her now!",
         "priority": 0,
         "deadline": "09.10.2023"
     },{
-        "taskName": "0002",
+        "taskName": "Organize the cellar",
         "image": "./images/walk-dog.jpg",
-        "description": "You need to take Fluffy for a walk. He needs to run and be with other dogs!",
+        "description": "You should start this week but you won't finish this year. It's a big task. Take your time but do it!",
+        "priority": 0,
+        "deadline": "01.02.2024"
+    },{
+        "taskName": "Read a few pages",
+        "image": "./images/walk-dog.jpg",
+        "description": "You can't find the time to red? Guess what, make time. It helps! Just do it!",
+        "priority": 0,
+        "deadline": "13.10.2023"
+    },{
+        "taskName": "Go for a walk",
+        "image": "./images/walk-dog.jpg",
+        "description": "You should go for a walk in the park. You need some fresh air. Always sitting inside isn't good for your health.",
         "priority": 0,
         "deadline": "09.10.2023"
     },{
-        "taskName": "0003",
+        "taskName": "Buy new clothes",
         "image": "./images/walk-dog.jpg",
-        "description": "You need to take Fluffy for a walk. He needs to run and be with other dogs!",
+        "description": "You need a new pair of shoes. Go buy them!",
         "priority": 0,
-        "deadline": "09.10.2023"
+        "deadline": "16.10.2023"
     },{
-        "taskName": "Take dog for a walk",
+        "taskName": "Work out",
         "image": "./images/walk-dog.jpg",
-        "description": "You need to take Fluffy for a walk. He needs to run and be with other dogs!",
+        "description": "It has been a while! Work out! Sweat! Feel it!",
         "priority": 0,
-        "deadline": "09.10.2023"
+        "deadline": "12.10.2023"
     },{
-        "taskName": "Take dog for a walk",
+        "taskName": "Meet friends",
         "image": "./images/walk-dog.jpg",
-        "description": "You need to take Fluffy for a walk. He needs to run and be with other dogs!",
+        "description": "Just call them and meet them. Have a good time!",
         "priority": 0,
-        "deadline": "09.10.2023"
-    },{
-        "taskName": "Take dog for a walk",
-        "image": "./images/walk-dog.jpg",
-        "description": "You need to take Fluffy for a walk. He needs to run and be with other dogs!",
-        "priority": 0,
-        "deadline": "09.10.2023"
-    },{
-        "taskName": "Take dog for a walk",
-        "image": "./images/walk-dog.jpg",
-        "description": "You need to take Fluffy for a walk. He needs to run and be with other dogs!",
-        "priority": 0,
-        "deadline": "09.10.2023"
+        "deadline": "16.10.2023"
     }]
 `
