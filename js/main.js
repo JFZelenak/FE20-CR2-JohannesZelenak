4fcd589f7deffc86b2bd69cdecf07947fbe84a55
Johannes Zelenak <jf.zelenak@gmail.com>
2023-10-06 10:26:08 +0200

    add changes

--- js/main.js
+++ js/main.js
@@ -4,17 +4,28 @@ let obj1 = JSON.parse(tasks);
 
 for(let i = 0; i < obj1.length; i++) {
     resultDiv.innerHTML += `    
-    // <div class="card" style="width: 18rem;">
-    //     <img src="${obj1[i].image}" class="card-img-top" alt="${obj1[i].taskName}">
-    //     <div class="card-body">
-    //         <p class="h5 card-title">${obj1[i].taskName}</p>
-    //         <p class="h6 card-text">${obj1[i].description}</p>
-    //         <hr>
-    //         <p class="h6 card-text">Priority level: ${obj1[i].priority}</p>
-    //         <p class="h6 card-text">Deadline: ${obj1[i].deadline}</p>
-    //         <a href="#" class="btn btn-primary">Go somewhere</a>
-    //     </div>
-    // </div>
+    <div class="card py-3 myCard">
+        <div class="d-flex justify-content-between">
+            <p class="h6">Task</p>
+            <div class="d-flex justify-content-end">
+                <i class="bi bi-bookmark"></i>
+                <i class="bi bi-three-dots-vertical ms-2"></i>
+            </div>
+        </div>
+        <img src="./images/walk-dog.jpg" class="img-thumbnail mb-2" alt="Take dog for a walk">
+        <p class="h5 card-title">Take dog for a walk</p>
+        <p class="h6 card-text">You need to take Fluffy for a walk. He needs to run and be with other dogs!</p>
+        <hr>
+        <p class="h6 card-text">
+            <i class="bi bi-exclamation-triangle-fill"></i> Priority level: <span class="rounded bg-success myBtn">0</span>
+        </p>
+        <p class="h6 card-text">Deadline: 09.10.2023</p>
+        <hr>
+        <div class="d-flex justify-content-end">
+            <a href="#" class="btn btn-danger ms-2"><i class="bi bi-trash"></i> Delete</a>
+            <a href="#" class="btn btn-success ms-2"><i class="bi bi-check-circle"></i> Done</a>
+        </div>
+    </div>
     `;
 }
 
